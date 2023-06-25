26c616900d0939c3148ff2d93affb0dca591885c
adityash15 <89626796+adityash15@users.noreply.github.com>
2023-06-25 22:41:44 +0530

    added final api

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-const express = require("express");
-const serverless = require("serverless");
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
-const router = express.Router();
-let port = process.env.PORT || 3000
 
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get("/", (req,res)=>{
-  res.send("Hello world");
-})
+app.set('view engine', 'ejs');
 
-app.post("/notification", (req,res)=>{
-  res.send(`notification received: ${req.body}`)
-})
+app.post('/api/final', async function (req, res) {
+  const decodedString = JSON.parse(Buffer.from(String(req.body.cres), 'base64').toString('utf8'));
+  console.log('inside final portion--------------', req.body.cres, decodedString);
+  // console.log("decoded", decodedString.merchantTransID, decodedString)
+  res.render('index', { data: decodedString.acsTransID });
+  res.json({ message: `Payment successful for finale ${decodedString}` });
+  res.send({message: "complete"})
+});
 
-// app.use('/.netlify/functions/api', router)
-// module.exports.handler = new serverless(app)
-
-app.listen(port, ()=>{
-  console.log(`example app is listening on port ${port}`)
-})
+// // Start the server
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
